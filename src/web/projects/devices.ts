379996cb3bcd3e0b535070512da51d9175cb6a17
Daniel Huisman <daniel@huisman.me>
2022-12-30 13:12:42 +0100

    Add Lattice Nexus devices

--- src/web/projects/devices.ts
+++ src/web/projects/devices.ts
@@ -32,16 +32,21 @@ export const VENDORS: Vendor[] = [
         packages: {
             /* eslint-disable @typescript-eslint/naming-convention */
 
+            'ASG256': 'ASG256 (9 x 9 mm)',
+            'BBG484': 'BBG484 (19 x 19 mm)',
+            'BFG484': 'BFG484 (23 x 23 mm)',
             'bg121': '121-ball caBGA (9 x 9 mm)',
             'bg121:4k': '121-ball caBGA (9 x 9 mm)',
             'caBGA256': '256 caBGA (14 x 14 mm)',
             'caBGA381': '381 caBGA (17 x 17 mm)',
+            'caBGA400': '400 caBGA (17 x 17 mm)',
             'caBGA554': '554 caBGA (23 x 23 mm)',
             'caBGA756': '756 caBGA (27 x 27 mm)',
             'cb81': '81-ball csBGA2 (5 x 5 mm)',
             'cb121': '121-ball csBGA (6 x 6 mm)',
             'cb132': '132-ball csBGA (8 x 8 mm)',
             'cb132:4k': '132-ball csBGA (8 x 8 mm)',
+            'CBG256': 'CBG256 (14 x 14 mm)',
             'cm36': '36-ball ucBGA (2.5 x 2.5 mm)',
             'cm49': '49-ball ucBGA (3 x 3 mm)',
             'cm81': '81-ball ucBGA (4 x 4 mm)',
@@ -52,7 +57,11 @@ export const VENDORS: Vendor[] = [
             'cm225:4k': '225-ball ucBGA (7 x 7 mm)',
             'ct121': '121-ball caBGA (9 x 9 mm)',
             'ct256': '256-ball caBGA (14 x 14 mm)',
+            'csfBGA121': '121 csfBGA (6 x 6 mm)',
             'csfBGA285': '285 csfBGA (10 x 10 mm)',
+            'csfBGA289': '289 csBGA (9.5 x 9.5 mm)',
+            'LFG672': 'LFG672 (27 x 27 mm)',
+            'QFN72': '72 QFN (10 x 10 mm)',
             'qn32': '32-pin QFN (5 x 5 mm)',
             'qn84': '84-pin QFNS (7 x 7 mm)',
             'swg16tr': '16-ball WLCSP (1.40 x 1.48 mm)',
@@ -185,6 +194,68 @@ export const VENDORS: Vendor[] = [
                     }
 
                     // NOTE: ECP5 Automative Devices are also listed on the Lattice website, but nextpnr does not appear to support those devices
+                ]
+            },
+            // {
+            //     id: 'machxo2',
+            //     name: 'MachXO2',
+            //     // NOTE: MachXO2 devices exist in the Project Trellis repository, but might not be supported by a nextpnr command
+            //     architecture: 'ecp5',
+            //     url: 'https://www.latticesemi.com/MachXO2',
+            //     devices: []
+            // },
+            {
+                id: 'crosslink-nx',
+                name: 'CrossLink-NX',
+                architecture: 'nexus',
+                url: 'https://www.latticesemi.com/CrossLink-NX',
+                devices: [
+                    {
+                        id: 'lifcl-17',
+                        name: 'LIFCL-17',
+                        device: 'LIFCL-17',
+                        // NOTE: WLCSP72 is also listed on the Lattice website, but nextpnr does not appear to support it for this device
+                        packages: ['QFN72', 'csfBGA121', 'caBGA256']
+                    },
+                    // NOTE: LIFCL-33 is also listed on the Lattice website, but nextpnr does not appear to support this device
+                    {
+                        id: 'lifcl-40',
+                        name: 'LIFCL-40',
+                        device: 'LIFCL-40',
+                        // NOTE: csfBGA121 and caBGA256 are also listed on the Lattice website, but nextpnr does not appear to support it for this device
+                        packages: ['QFN72', 'csfBGA289', 'caBGA400']
+                    }
+                ]
+            },
+            {
+                id: 'certus-nx',
+                name: 'Certus-NX',
+                architecture: 'nexus',
+                url: 'https://www.latticesemi.com/Certus-NX',
+                devices: [
+                    // NOTE: LFD2NX-17 is also listed on the Lattice website, but nextpnr does not appear to support this device
+                    {
+                        id: 'lfd2nx-40',
+                        name: 'LFD2NX-40',
+                        device: 'LFD2NX-40',
+                        // NOTE: csfBGA121 and caBGA196 are also listed on the Lattice website, but nextpnr does not appear to support it for this device
+                        packages: ['caBGA256']
+                    }
+                ]
+            },
+            {
+                id: 'certuspro-nx',
+                name: 'CertusPro-NX',
+                architecture: 'nexus',
+                url: 'https://www.latticesemi.com/CertusPro-NX',
+                devices: [
+                    // NOTE: LFCPNX-50 is also listed on the Lattice website, but nextpnr does not appear to support this device
+                    {
+                        id: 'lfcpnx-100',
+                        name: 'LFCPNX-100',
+                        device: 'LFCPNX-100',
+                        packages: ['ASG256', 'CBG256', 'BBG484', 'BFG484', 'LFG672']
+                    }
                 ]
             }
         ]
