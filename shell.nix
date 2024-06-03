7fb25e045aa802f1af1746f7065ad49c27948eb0
Mike A. <git@mikealmel.ooo>
2024-06-03 21:30:47 +0200

    direnv: Fix frequent reloading

--- shell.nix
+++ shell.nix
@@ -7,7 +7,19 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
-    echo "Installing Node.js dependencies..."
-    npm i
+    CACHE_DIR=".direnv"
+    CACHE_PATH="$CACHE_DIR/npm_cache"
+
+    hash () { sha256sum $1 | awk '{print $1}'; }
+    
+    cur_hash="$(hash package.json)-$(hash package-lock.json)"
+
+    if [[ ! -f "$CACHE_PATH" || "$(cat $CACHE_PATH)" != $cur_hash ]]; then
+      echo "Installing Node.js dependencies..."
+      npm i
+
+      cur_hash="$(hash package.json)-$(hash package-lock.json)"
+      echo "$cur_hash" > $CACHE_PATH
+    fi
   '';
 }
