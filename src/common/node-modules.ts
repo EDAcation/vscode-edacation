ae440b04ad3ce960e06afc2c34a9cccf0c71b55e
Mike A. <git@mikealmel.ooo>
2024-07-02 16:01:51 +0200

    fix: node module importing

--- src/common/node-modules.ts
+++ src/common/node-modules.ts
@@ -1,30 +1,20 @@
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 type Module = any; // TODO: refine?
 
 const moduleCache = new Map<string, Module>();
 
-export const importSync = (module: string): Module => {
+export const importModule = (module: string): Module => {
     const cached = moduleCache.get(module);
     if (cached) return cached;
 
-    console.log(`Importing node module (!sync!): ${module}`);
+    console.log(`Importing node module: ${module}`);
 
     const mod = __non_webpack_require__(module);
-    moduleCache.set(module, mod);
-    return mod;
-};
-
-export const importAsync = async (module: string): Promise<Module> => {
-    const cached = moduleCache.get(module);
-    if (cached) return cached;
-
-    console.log(`Importing node module (async): ${module}`);
-
-    let mod;
-    try {
-        mod = await import(/* webpackIgnore: true */ module);
-    } catch {
-        throw new Error(`Could not import module: ${module}`);
-    }
     moduleCache.set(module, mod);
     return mod;
 };
@@ -39,10 +29,10 @@ export type ModuleTarFS = typeof import('tar-fs');
 export type ModuleWorkerThreads = typeof import('worker_threads');
 export type ModuleZLib = typeof import('zlib');
 
-export const childProcess = async () => importAsync('child_process') as Promise<ModuleChildProcess>;
-export const fs = async () => importAsync('fs') as Promise<ModuleFS>;
-export const os = async () => importAsync('os') as Promise<ModuleOS>;
-export const stream = async () => importAsync('stream') as Promise<ModuleStream>;
-export const tar = async () => importAsync('tar-fs') as Promise<ModuleTarFS>;
-export const workerThreads = async () => importAsync('worker_threads') as Promise<ModuleWorkerThreads>;
-export const zlib = async () => importAsync('zlib') as Promise<ModuleZLib>;
+export const childProcess = async () => importModule('child_process') as ModuleChildProcess;
+export const fs = async () => importModule('fs') as ModuleFS;
+export const os = async () => importModule('os') as ModuleOS;
+export const stream = async () => importModule('stream') as ModuleStream;
+export const tar = async () => importModule('tar-fs') as ModuleTarFS;
+export const workerThreads = async () => importModule('worker_threads') as ModuleWorkerThreads;
+export const zlib = async () => importModule('zlib') as ModuleZLib;
