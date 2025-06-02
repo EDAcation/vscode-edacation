8af807696d2ef5d9b053b47c06a9b0b9d52cabb0
Mike A. <git@mikealmel.ooo>
2025-06-02 17:04:52 +0200

    chore: Remove leftover comments

--- src/views/actions/App.vue
+++ src/views/actions/App.vue
@@ -18,23 +18,6 @@ export default defineComponent({
             projectState
         };
     }
-    // methods: {
-    //     message(event: MessageEvent) {
-    //         console.log('message', event.data);
-
-    //         switch (event.data.type) {
-    //             case 'project':
-    //                 this.state.project = event.data.project;
-
-    //                 const targetCount = this.state.project?.configuration.targets.length ?? 0;
-    //                 if (this.state.selectedTargetIndex < 0) this.state.selectedTargetIndex = 0;
-    //                 if (this.state.selectedTargetIndex > targetCount - 1)
-    //                     this.state.selectedTargetIndex = targetCount - 1;
-
-    //                 break;
-    //         }
-    //     }
-    // }
 });
 </script>
 
