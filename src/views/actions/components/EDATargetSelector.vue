04b2e39b9ec93a93105ab22501a62fa908395a02
Mike A. <git@mikealmel.ooo>
2025-09-18 23:21:25 +0200

    refactor: use target id to track targets instead of index

--- src/views/actions/components/EDATargetSelector.vue
+++ src/views/actions/components/EDATargetSelector.vue
@@ -1,17 +1,12 @@
 <script lang="ts">
 import {VscodeSingleSelect} from '@vscode-elements/elements';
-import {type TargetConfiguration} from 'edacation';
+import {ProjectTarget} from 'edacation';
 import {defineComponent} from 'vue';
 
 import {syncedState as projectState} from '../../project';
 import {state as globalState} from '../state';
 
 export default defineComponent({
-    props: {
-        targetIndex: {
-            type: Number
-        }
-    },
     data() {
         return {
             state: globalState,
@@ -19,23 +14,32 @@ export default defineComponent({
         };
     },
     computed: {
-        targets(): TargetConfiguration[] {
-            return this.projectState.project?.getConfiguration().targets ?? [];
+        targetId(): string | undefined {
+            return this.state.selectedTargetId;
+        },
+        targets(): ProjectTarget[] {
+            return this.projectState.project?.getTargets() ?? [];
+        },
+        selectedTarget(): ProjectTarget | null {
+            if (this.targetId === undefined) {
+                return this.targets[0] ?? null;
+            }
+            return this.projectState.project?.getTarget(this.targetId) ?? null;
         }
     },
     methods: {
         handleTargetChange(event: Event) {
             if (!event.target) return;
 
-            this.state.selectedTargetIndex = (event.target as VscodeSingleSelect).selectedIndex;
+            this.state.selectedTargetId = (event.target as VscodeSingleSelect).value;
         }
     }
 });
 </script>
 
 <template>
-    <vscode-single-select style="width: initial" @change="handleTargetChange">
-        <vscode-option v-for="(target, index) in targets" :selected="index === targetIndex">
+    <vscode-single-select style="width: initial" :value="selectedTarget?.id" @change="handleTargetChange">
+        <vscode-option v-for="target in targets" :value="target.id">
             {{ target.name }}
         </vscode-option>
     </vscode-single-select>
--- src/views/actions/components/EDATargetTLM.vue
+++ src/views/actions/components/EDATargetTLM.vue
@@ -1,17 +1,15 @@
 <script lang="ts">
-import {VscodeTextfield} from '@vscode-elements/elements';
-import {type TargetConfiguration, type YosysOptions, getYosysDefaultOptions, getYosysOptions} from 'edacation';
+/* global NodeJS, setTimeout, clearTimeout */
+import type {VscodeTextfield} from '@vscode-elements/elements';
+import {ProjectTarget, type YosysOptions, getYosysDefaultOptions, getYosysOptions} from 'edacation';
 import {defineComponent} from 'vue';
 
 import {syncedState as projectState} from '../../project';
 import {state as globalState} from '../state';
 
+let debounceTimer: NodeJS.Timeout | undefined = undefined;
+
 export default defineComponent({
-    props: {
-        targetIndex: {
-            type: Number
-        }
-    },
     data() {
         return {
             state: globalState,
@@ -19,24 +17,27 @@ export default defineComponent({
         };
     },
     computed: {
-        target(): TargetConfiguration | null {
-            if (this.targetIndex === undefined) return null;
-
-            const targets = this.projectState.project?.getConfiguration().targets ?? [];
-            return targets[this.targetIndex] || null;
+        targets(): ProjectTarget[] {
+            return this.projectState.project?.getTargets() ?? [];
         },
+        selectedTarget(): ProjectTarget | null {
+            if (this.state.selectedTargetId === undefined) {
+                return this.targets[0] ?? null;
+            }
+            return this.projectState.project?.getTarget(this.state.selectedTargetId) ?? null;
+        },
+
         effectiveOptions(): YosysOptions | null {
             if (!this.projectState.project) return null;
 
             const projectConfig = this.projectState.project.getConfiguration();
-            const targetId = this.target?.id;
 
-            if (!targetId) {
+            if (!this.selectedTarget) {
                 // Default configuration
                 return getYosysDefaultOptions(projectConfig);
             } else {
                 // Target configuration
-                return getYosysOptions(projectConfig, targetId);
+                return getYosysOptions(projectConfig, this.selectedTarget.id);
             }
         },
         topLevelModule(): string | null {
@@ -45,10 +46,18 @@ export default defineComponent({
     },
     methods: {
         handleTLMChange(event: Event) {
-            if (!this.target) return;
+            if (!this.selectedTarget) return;
 
             const newTlm = (event.target as VscodeTextfield).value;
-            projectState.project?.setTopLevelModule(this.target.id, newTlm);
+            if (debounceTimer !== undefined) {
+                clearTimeout(debounceTimer);
+            }
+            debounceTimer = setTimeout(() => {
+                if (this.selectedTarget) {
+                    this.projectState.project?.setTopLevelModule(this.selectedTarget.id, newTlm);
+                }
+                debounceTimer = undefined;
+            }, 300);
         }
     }
 });
