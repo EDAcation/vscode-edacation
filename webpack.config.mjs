6c1512208df89e0b84cc5b300fb32bae4cc8ad69
Mike A. <git@mikealmel.ooo>
2026-04-07 17:24:00 +0200

    fix: improve vue treeshaking

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -232,6 +232,11 @@ const viewsConfig = Object.assign({}, baseConfig, {
     },
     plugins: [
         new VueLoaderPlugin(),
+        new webpack.DefinePlugin({
+            __VUE_OPTIONS_API__: 'true',
+            __VUE_PROD_DEVTOOLS__: 'false',
+            __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+        }),
         new MiniCssExtractPlugin({
             filename: '[name]/index.css'
         }),
